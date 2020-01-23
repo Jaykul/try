635df9c8a308b203f52a34e52193ee1d7ecf5bb1
Joel Bennett <Jaykul@HuddledMasses.org>
2020-01-23 01:01:43 -0500

    Clean up build-and-install-dotnet-interactive

--- dotnet-interactive/build-and-install-dotnet-interactive.ps1
+++ dotnet-interactive/build-and-install-dotnet-interactive.ps1
@@ -1,23 +1,26 @@
 Set-StrictMode -version 2.0
 $ErrorActionPreference = "Stop"
 
-$thisDir = Split-Path -Parent $PSCommandPath
 $toolLocation = ""
 $toolVersion = ""
 if (Test-Path 'env:DisableArcade') {
-    dotnet pack "$thisDir\dotnet-interactive.csproj" /p:Version=0.0.0
-    $script:toolLocation = "$thisDir\bin\debug"
+    Write-Host dotnet pack "$PSScriptRoot\dotnet-interactive.csproj" /p:Version=0.0.0
+    dotnet pack "$PSScriptRoot\dotnet-interactive.csproj" /p:Version=0.0.0
+    $script:toolLocation = "$PSScriptRoot\bin\debug"
     $script:toolVersion = "0.0.0"
 } else {
     if ($IsLinux -or $IsMacOS) {
-        & "$thisDir\..\build.sh" --pack
+        Write-Host (Convert-Path "$PSScriptRoot\..\build.sh")
+        & "$PSScriptRoot\..\build.sh" --pack
     } else {
-        & "$thisDir\..\build.cmd" -pack
+        Write-Host (Convert-Path "$PSScriptRoot\..\eng\build.ps1")
+        & "$PSScriptRoot\..\eng\build.ps1" -build -binaryLog -pack
     }
 
-    $script:toolLocation = "$thisDir\..\artifacts\packages\Debug\Shipping"
+    $script:toolLocation = "$PSScriptRoot\..\artifacts\packages\Debug\Shipping"
     $script:toolVersion = "1.0.44142.42"
 }
-
+Write-Host "uninstall and reinstall dotnet-interactive"
+dotnet tool uninstall -g dotnet-interactive
 dotnet tool uninstall -g Microsoft.dotnet-interactive
 dotnet tool install -g --add-source "$toolLocation" --version $toolVersion Microsoft.dotnet-interactive
